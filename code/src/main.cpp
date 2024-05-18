5f91e131137c02670786f477cd45382916f450ea
TillWege <tillwege@gmail.com>
2024-05-18 23:26:11 +0200

    fix trail rendering

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <iostream>
 #include <stack>
-
+#include "vector"
 #define SCREEN_WIDTH (1600)
 #define SCREEN_HEIGHT (900)
 
@@ -12,78 +12,98 @@
 
 const int numPoints = 3;      // Define the number of points
 Vector2 points[numPoints] = { // Correct array declaration
-    {0, 0},
-    {0, SCREEN_HEIGHT},
-    {SCREEN_WIDTH, SCREEN_HEIGHT}};
+	{ 0, 0 },
+	{ 0, SCREEN_HEIGHT },
+	{ SCREEN_WIDTH, SCREEN_HEIGHT }};
 
-const int trailSize = 100;
-Vector2 trail[trailSize];
 
-int trailIndex = 0;
+struct Trail {
+	Vector2* data;
+	int size;
+	int top;
+
+	Trail(int size) {
+		data = new Vector2[size];
+		this->size = size;
+		top = 0;
+	}
+
+	~Trail() {
+		delete[] data;
+	}
+
+	void push(Vector2 point) {
+		if(top < size) {
+			while (top < size) {
+				data[top] = point;
+				top++;
+			}
+		} else {
+			memcpy(&data[0], &data[1], (size - 1) * sizeof(Vector2));
+			data[top - 1] = point;
+		}
+	}
+
+};
+
+Trail earthTrail = Trail(100);
 
 int main(void)
 {
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
+	SetConfigFlags(FLAG_MSAA_4X_HINT);
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+	SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH "/test.png"); // Check README.md for how this works
+	Texture2D texture = LoadTexture(ASSETS_PATH "/test.png"); // Check README.md for how this works
 
-    rlImGuiSetup(true);
+	rlImGuiSetup(true);
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        rlImGuiBegin();
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		rlImGuiBegin();
 
-        ClearBackground(BLACK);
+		ClearBackground(BLACK);
 
-         const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-         DrawTexture(texture, texture_x, texture_y, WHITE);
+		const char* text = "Planet Sim";
+		const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
 
-         const char *text = "OMG! IT WORKS!";
-         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+		DrawText(text,
+			SCREEN_WIDTH / 2 - text_size.x / 2,
+			30,
+			20,
+			RAYWHITE);
 
-         bool open = true;
-         ImGui::ShowDemoWindow(&open);
+		// Sun
+		DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 100, YELLOW);
 
-         DrawSplineBezierQuadratic(points, numPoints, 4.0f, BLUE);
+		// Earth
 
-        // Sun
-        DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 100, YELLOW);
+		float angle = GetTime() * 0.5f;
 
-        // Earth
+		float earth_x = SCREEN_WIDTH / 2 + 200 * cos(angle);
+		float earth_y = SCREEN_HEIGHT / 2 + 200 * sin(angle);
 
-        float angle = GetTime() * 0.5f;
+		earthTrail.push({ earth_x, earth_y });
 
-        float earth_x = SCREEN_WIDTH / 2 + 200 * cos(angle);
-        float earth_y = SCREEN_HEIGHT / 2 + 200 * sin(angle);
+		DrawSplineLinear(earthTrail.data, earthTrail.size, 2.0f, SKYBLUE);
 
-		trail[trailIndex] = {earth_x, earth_y};
-		trailIndex = (trailIndex + 1) % trailSize;
+		DrawCircle(earth_x, earth_y, 20, BLUE);
 
-		if(trailIndex == 0)
-			std::cout << "Trail reset" << std::endl;
+		ImGui::Begin("TrailDebug");
+		{
+			for (int i = 0; i < earthTrail.size; i++) {
+				ImGui::Text("Trail[%d]: (%f, %f)", i, earthTrail.data[i].x, earthTrail.data[i].y);
+			}
+		}
+		ImGui::End();
 
+		rlImGuiEnd();
+		EndDrawing();
+	}
 
+	rlImGuiShutdown();
+	CloseWindow();
 
-		DrawSplineLinear(trail, trailIndex, 2.0f, SKYBLUE);
-		DrawSplineLinear(trail + trailIndex, trailSize - trailIndex, 2.0f, SKYBLUE);
-
-
-		DrawSplineLinear(trail, trailSize, 2.0f, SKYBLUE);
-
-        DrawCircle(earth_x, earth_y, 20, BLUE);
-
-
-        rlImGuiEnd();
-        EndDrawing();
-    }
-
-    rlImGuiShutdown();
-    CloseWindow();
-
-    return 0;
+	return 0;
 }
