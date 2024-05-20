75a2394aeca2b6eaaeb327560198bfaef47def80
TillWege <tillwege@gmail.com>
2024-05-20 21:04:55 +0200

    basic 3D Rendering

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <iostream>
 #include <stack>
+#include <string>
+#include <utility>
 #include "vector"
 #define SCREEN_WIDTH (1600)
 #define SCREEN_HEIGHT (900)
@@ -16,54 +18,158 @@ Vector2 points[numPoints] = { // Correct array declaration
 	{ 0, SCREEN_HEIGHT },
 	{ SCREEN_WIDTH, SCREEN_HEIGHT }};
 
-
-class Trail {
-public:
-	Trail(int size) {
-		data = new Vector2[size];
-		this->size = size;
-		this->top = 0;
-	}
-
-	~Trail() {
-		delete[] data;
-	}
-
-	void push(Vector2 point) {
-		if(this->top < this->size) {
-			this->data[this->top] = point;
-			this->top++;
-		} else {
-			memcpy(&this->data[0], &this->data[1], (this->size - 1) * sizeof(Vector2));
-			this->data[top - 1] = point;
-		}
-	}
-
-	Vector2* get() {
-		return this->data;
-	}
-
-	int getTop() {
-		return this->top;
-	}
-
-	Vector2 getPoint(int index) {
-		return this->data[index];
-	}
-
-	int getSize() {
-		return this->size;
-	}
-
-
-private:
-	Vector2* data;
-	int size;
-	int top;
-
+enum CursorState
+{
+	Uninitialized, Locked, Unlocked
 };
 
-Trail* earthTrail = NULL;
+CursorState cursorState = Uninitialized;
+
+void updateCursor()
+{
+	if (!IsCursorHidden() && cursorState == Locked)
+	{
+		DisableCursor();
+	}
+
+	if (cursorState == Uninitialized)
+	{
+		DisableCursor();
+		cursorState = Locked;
+	}
+
+	if (IsKeyPressed(KEY_F1))
+	{
+		if (cursorState == Locked)
+		{
+			EnableCursor();
+			cursorState = Unlocked;
+		}
+		else
+		{
+			DisableCursor();
+			cursorState = Locked;
+		}
+	}
+}
+
+struct GraphicsDebugger
+{
+	bool showWireframe = false;
+	bool showGrid = false;
+	int gridSize = 10;
+	int maxFPS = 100;
+};
+
+GraphicsDebugger graphicsDebugger;
+
+void GraphicsDebuggerUI()
+{
+	ImGui::Begin("Graphics Debugger", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+	{
+
+		ImGui::Checkbox("Show Wireframe", &graphicsDebugger.showWireframe);
+		ImGui::Checkbox("Show Grid", &graphicsDebugger.showGrid);
+		ImGui::SliderInt("Grid Size", &graphicsDebugger.gridSize, 1, 100);
+		ImGui::Separator();
+		ImGui::Text("Fps: %d", GetFPS());
+		ImGui::Text("Frame Time: %f", GetFrameTime());
+		int prevMaxFPS = graphicsDebugger.maxFPS;
+		ImGui::SliderInt("Max FPS", &graphicsDebugger.maxFPS, 1, 1000);
+		if (prevMaxFPS != graphicsDebugger.maxFPS)
+		{
+			SetTargetFPS(graphicsDebugger.maxFPS);
+		}
+
+	}
+	ImGui::End();
+}
+
+struct Body
+{
+	std::string name;
+	float orbitHeight;
+	float angle;
+	float radius;
+	float mass;
+	Color color;
+	struct
+	{
+		std::vector<Vector3> data;
+		size_t maxCount = 100;
+	} trail;
+
+	void appendTrail(Vector3 point)
+	{
+		if (trail.data.size() >= trail.maxCount)
+		{
+			trail.data.erase(trail.data.begin());
+		}
+		trail.data.push_back(point);
+	}
+
+	void updateTrailSize(size_t size)
+	{
+		trail.maxCount = size;
+		trail.data.clear();
+		trail.data.reserve(size);
+	}
+
+	size_t trailCount()
+	{
+		return trail.data.size();
+	}
+};
+
+void DrawBody(Body& body)
+{
+	Vector3 pos = { 0, 0, 0 };
+	if (body.orbitHeight > 0)
+	{
+		pos.x = body.orbitHeight * cos(body.angle);
+		pos.z = body.orbitHeight * sin(body.angle);
+	}
+
+	body.appendTrail(pos);
+
+	if (graphicsDebugger.showWireframe)
+		DrawSphereWires(pos, body.radius, 10, 10, body.color);
+	else
+		DrawSphere(pos, body.radius, body.color);
+
+	for (size_t i = 1; i < body.trailCount(); i++)
+	{
+		DrawLine3D(body.trail.data[i - 1], body.trail.data[i], body.color);
+	}
+
+}
+
+void BodyDebuggerUI(Body& body)
+{
+
+	std::string windowName = "Body Debugger: " + body.name;
+
+	ImGui::Begin(windowName.c_str(), NULL, ImGuiWindowFlags_AlwaysAutoResize);
+	{
+		ImGui::Text("Orbit Height: %f", body.orbitHeight);
+		ImGui::Text("Angle: %f°", body.angle);
+		ImGui::Text("Radius: %f", body.radius);
+		ImGui::Text("Mass: %f", body.mass);
+
+		ImGui::BeginChild("Trail", { 300, 300 });
+
+		for (size_t i = 0; i < body.trailCount(); i++)
+		{
+			Vector3& point = body.trail.data[i];
+			ImGui::Text("%d: x: %f, y: %f, z: %f", i, point.x, point.y, point.z);
+		}
+
+		ImGui::EndChild();
+	}
+	ImGui::End();
+}
+
+std::vector<Body> bodies;
 
 int main(void)
 {
@@ -71,17 +177,34 @@ int main(void)
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
 	SetTargetFPS(60);
 
-	Texture2D texture = LoadTexture(ASSETS_PATH "/test.png"); // Check README.md for how this works
-
+	//Texture2D texture = LoadTexture(ASSETS_PATH "/test.png"); // Check README.md for how this works
 	rlImGuiSetup(true);
 
-	earthTrail = new Trail(100);
+
+	Camera camera = { 0 };
+	camera.projection = CAMERA_PERSPECTIVE;
+	camera.fovy = 45.0f;
+	camera.position = { 0.0f, 10.0f, 10.0f };
+	camera.target = { 0.0f, 0.0f, 0.0f };
+	camera.up = { 0.0f, 1.0f, 0.0f };
+
+	SetExitKey(0);
+
+	SetWindowFocused();
+
+	Body sun = { "Sun", 0.0f, 0.0f, 2.0f, 10.0f, YELLOW};
+	bodies.push_back(sun);
+
+	Body earth = { "Earth", 7.0f, 0.0f, 0.5f, 0.1f, BLUE};
+	bodies.push_back(earth);
 
 	while (!WindowShouldClose())
 	{
+		updateCursor();
+
+
 		BeginDrawing();
 		rlImGuiBegin();
-
 		ClearBackground(BLACK);
 
 		const char* text = "Planet Sim";
@@ -91,56 +214,34 @@ int main(void)
 			SCREEN_WIDTH / 2 - text_size.x / 2,
 			30,
 			20,
-			RAYWHITE);
+			RAYWHITE
+		);
 
-		// Sun
-		DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 100, YELLOW);
-
-		// Earth
-
-		float angle = GetTime() * 0.5f;
-
-		float earth_x = SCREEN_WIDTH / 2 + 200 * cos(angle);
-		float earth_y = SCREEN_HEIGHT / 2 + 200 * sin(angle);
-
-		earthTrail->push({ earth_x, earth_y });
-
-		DrawSplineLinear(earthTrail->get(), earthTrail->getTop(), 2.0f, SKYBLUE);
-
-		DrawCircle(earth_x, earth_y, 20, BLUE);
-
-
-		ImGui::Begin("Target Debug", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+		for (Body& body : bodies)
 		{
-			ImGui::Text("Currently Selected: Earth");
-
-			int trailSize = earthTrail->getSize();
-			ImGui::SliderInt("Trail Size", &trailSize, 1, 100);
-
-			if (trailSize != earthTrail->getSize()) {
-				delete earthTrail;
-				earthTrail = new Trail(trailSize);
-			}
-
-			if(ImGui::Button("Reset Trail")) {
-				delete earthTrail;
-				earthTrail = new Trail(100);
-			}
-
-
-			ImGui::Text("Earth Trail");
-
-			ImGui::BeginChild("Scrolling", {0, 300}, ImGuiChildFlags_Border);
-
-			for (int i = 0; i < earthTrail->getTop(); i++) {
-				auto point = earthTrail->getPoint(i);
-				ImGui::Text("Point %d: (%f, %f)", i, point.x, point.y);
-			}
-
-			ImGui::EndChild();
-
+			body.angle += GetFrameTime() * 0.5f;
+			BodyDebuggerUI(body);
+//			earthTrail->push({body.orbitHeight * cos(body.angle), 0, body.orbitHeight * sin(body.angle)});
 		}
-		ImGui::End();
+
+
+		BeginMode3D(camera);
+
+		if (cursorState == Locked)
+			UpdateCamera(&camera, CAMERA_FREE);
+
+
+		if (graphicsDebugger.showGrid)
+			DrawGrid(graphicsDebugger.gridSize, 1.0f);
+
+		for (Body& body : bodies)
+		{
+			DrawBody(body);
+			BodyDebuggerUI(body);
+		}
+
+		EndMode3D();
+		GraphicsDebuggerUI();
 
 		rlImGuiEnd();
 		EndDrawing();
@@ -151,3 +252,5 @@ int main(void)
 
 	return 0;
 }
+
+
